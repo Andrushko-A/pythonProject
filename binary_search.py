d7174c4be7a30bf8c08c6450b8ac82b5c623cfd1
andru <andrushkoanna2003@gmail.com>
2022-11-08 09:18:09 +0300

    python

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,8 @@
 def binary_search(list, item):
-    low=0
-    high=len(list)-1
+    low=-1
+    high=len(list)
 
-    while low<=high:
+    while high-low>1:
         mid=int((low+high)/2)
         guess=list[mid]
         if guess==item:
--- sumNeighbors.py
+++ sumNeighbors.py
@@ -1,12 +1,11 @@
 def search(arr, x):
     for i in range(0, len(arr)):
-        if (arr[0]+arr[i]==x):
-            return arr[i]
+        j=i+1
+        for j in range(0, len(arr)):
+            if (arr[i]+arr[j]==x):
+                return arr[j]
     return -1
 arr = [-1, 2, 3, 7, 10]
-x=1
-arr1=[]
+x=9
 result=search(arr, x)
-arr1.append(arr[0])
-arr1.append(result)
-print(arr1)
+print(result)
