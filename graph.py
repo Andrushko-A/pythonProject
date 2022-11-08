814a17021156c1e9ccd55dff351a64b474f8dea0
andru <andrushkoanna2003@gmail.com>
2022-11-08 23:04:44 +0300

    python

---
+++ graph.py
@@ -0,0 +1,14 @@
+a, b, c, d, e, f, g, h = range(8)
+N= [
+    {b, c, d, e, f}, #a
+    {c, e}, #b
+    {d}, #c
+    {e}, #d
+    {f}, #e
+    {c, g, h}, #f
+    {f, h}, #g
+    {f, g} #h
+]
+print(len(N[f]))
+print(b in N[a])
+
